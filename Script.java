07d3e9deb46f38afc932d2ec6625276183398fe3
MaiNgo95 <91213817+MaiNgo95@users.noreply.github.com>
2021-09-22 12:02:36 -0500

    Create Script.java

---
+++ Script.java
@@ -0,0 +1,27 @@
+//create a function to reverse a string
+//abcd -> dcba
+function reverse(str){
+  //need to check input first
+  //if there is no string
+  // converse the string into an array
+  if (!str || str.length < 2 || typeof str != 'string'){
+    return "that is not a string";
+  } //check all the condition of a string
+   const list =[];
+   const totalItems = str.length-1; //get the number without 0 
+   //loop through the string to get every element
+//put i = the last item in the list
+   for (let i = totalItems; i >= 0; i--){
+      //add the item after loop into trhe list
+      list.push(str[i]); //str[i] to get the element not the index
+    
+   }
+  console.log(list);
+  //return list.join('');
+}
+function reverse1(str){ 
+  console.log(str.split("").reverse().join('')); //second method
+}
+reverse('abcdef')
+reverse("maingo")
+reverse1("computer")
